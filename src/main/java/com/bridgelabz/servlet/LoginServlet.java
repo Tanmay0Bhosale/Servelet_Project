958c0c7d4875507a147ff537ac4833d9c0c49a93
Tanmay <tanmaytanvi1234@gmail.com>
2026-03-10 00:40:31 +0530

    [Tanmay] : Added UC3 : Implemented Username Validation

--- src/main/java/com/bridgelabz/servlet/LoginServlet.java
+++ src/main/java/com/bridgelabz/servlet/LoginServlet.java
@@ -14,10 +14,27 @@ public class LoginServlet extends HttpServlet {
                           HttpServletResponse response)
             throws ServletException, IOException {
 
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
-        if(user.equals("Tanmay") && pwd.equals("Password123")){
+        // UC3 Username validation
+        if(!user.matches("[A-Z][a-zA-Z]{2,}")){
+
+            out.println("<h3>Invalid Username</h3>");
+            out.println("<p>Name must start with capital letter and have minimum 3 characters</p>");
+
+            request.getRequestDispatcher("login.html")
+                    .include(request,response);
+
+            return;
+        }
+
+        // UC2 Login Check
+        if(user.equals("Tanmay") && pwd.equals("Password1@")){
 
             request.setAttribute("user", user);
 
@@ -25,8 +42,6 @@ public class LoginServlet extends HttpServlet {
                     .forward(request,response);
         }
         else{
-
-            PrintWriter out = response.getWriter();
 
             out.println("<h3>Invalid username or password</h3>");
 
